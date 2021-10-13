76daf7d00a7bb5b2ebeff1e9d4a2b26243784a46
Jurga <jurgak@gmail.com>
2021-10-13 17:18:17 +0300

    Array assignment 3 added

---
+++ src/com/company/gradeArray.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class gradeArray {
+
+    public static void main(String[] args) {
+        byte[] gradeArray = new byte[10];
+        fillArray(gradeArray);
+        failedOrA(gradeArray);
+        histogram(gradeArray);
+    }
+
+    static void fillArray(byte[] gradeArray) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("You will have to enter the grades for " + gradeArray.length + " students in total.");
+        int i;
+        for (i = 0; i < gradeArray.length; i++) {
+            byte grade;
+            int studentNo = i + 1;
+            do {
+                System.out.print("Please enter the grade for Student " + studentNo + ": ");
+                while (!input.hasNextByte()) {
+                    System.out.print("That's not a number! Enter again: ");
+                    input.next();
+                }
+                grade = input.nextByte();
+                if (grade < 0 || grade > 10)
+                    System.out.print("Grade must be between 0 and 10. Try again. \n ");
+            } while (grade < 0 || grade > 10);
+            gradeArray[i] = grade;
+        }
+
+    }
+
+    static void failedOrA(byte[] gradeArray) {
+        int counterF = 0;
+        int counterA = 0;
+        for (byte b : gradeArray) {
+            if (b < 4) {
+                counterF++;
+            }
+            if (b == 10) {
+                counterA++;
+            }
+        }
+        System.out.println("The number of students who failed is: " + counterF);
+        System.out.println("The number of students who got A is: " + counterA);
+    }
+
+    static void histogram(byte[] gradeArray) {
+        byte gradeV = 0;
+        while (gradeV <= 10) {
+            int studentCount = 0;
+            for (byte grade : gradeArray) {
+                if (grade == gradeV) {
+                    studentCount++;
+                }
+            }
+            System.out.println(studentCount + " students with the grade " + gradeV);
+            gradeV++;
+        }
+    }
+}
+
